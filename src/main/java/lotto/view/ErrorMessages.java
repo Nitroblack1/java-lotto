57619721ccd27e3fa689b7c5691da7c181911dfb
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-12-03 17:52:19 +0900

    feat(ErrorMessages, PrintOuts) : implement printout constants

---
+++ src/main/java/lotto/view/ErrorMessages.java
@@ -0,0 +1,6 @@
+package lotto.view;
+
+public class ErrorMessages {
+    public static final String MONEY_SHOULD_1_000_UNIT = "[ERROR] 입력 금액은 천원단위여야 합니다.";
+    public static final String LOTTO_NUMBER_WRONG_FORMAT = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+}

987ba503f07525a77278f229bca12df1dc45e470
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-12-03 17:43:46 +0900

    chore : add package following MVC Pattern and move application class, lotto class to appropriate package

--- src/test/java/lotto/controllerTest/ApplicationTest.java
+++ src/test/java/lotto/controllerTest/ApplicationTest.java
@@ -1,6 +1,7 @@
-package lotto;
+package lotto.controllerTest;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.controller.Application;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;

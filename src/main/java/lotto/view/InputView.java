16cfd510a5de8998e4a4b04e6b96732347aba0bc
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-12-03 18:18:55 +0900

    feat(InputView) : implement one of InputView method
    - get money input from user and validate it

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,28 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public InputView() {
+        System.out.println(PrintOuts.INPUT_MONEY);
+    }
+
+    public int inputMoney(String userInput) {
+        return Integer.parseInt(validateMoneyInput(userInput));
+    }
+
+    private String validateMoneyInput(String userInput) throws IllegalArgumentException {
+        String pattern = "^[0-9]*$";
+        if (!userInput.matches(pattern)) {
+            throw new IllegalArgumentException(ErrorMessages.MONEY_SHOULD_1_000_UNIT);
+        }
+        if (Integer.parseInt(userInput) % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessages.MONEY_SHOULD_1_000_UNIT);
+        }
+        if (Integer.parseInt(userInput) >= 2_000_000_000) {
+            throw new IllegalArgumentException(ErrorMessages.MONEY_SHOULD_SMALLER_THAN_2BILLION);
+        }
+        return userInput;
+    }
 }

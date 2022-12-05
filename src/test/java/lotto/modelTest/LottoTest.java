de44efb8951bc38cb7801a37a1c38bc9c3475c7a
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-12-05 16:09:13 +0900

    feat(LottoTest) : implement test for lotto system

--- src/test/java/lotto/modelTest/LottoTest.java
+++ src/test/java/lotto/modelTest/LottoTest.java
@@ -1,11 +1,15 @@
 package lotto.modelTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import lotto.model.Lotto;
+import lotto.model.WinNumberStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -19,10 +23,23 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 당첨 결과 테스트")
+    @Test
+    void returnWinNumber() {
+        List<List<Integer>> mockedTickets = new ArrayList<>();
+        mockedTickets.add(Arrays.asList(1,2,3,4,5,6));
+        mockedTickets.add(Arrays.asList(1,3,5,7,9,11));
+        mockedTickets.add(Arrays.asList(1,4,7,10,13,16));
+        mockedTickets.add(Arrays.asList(2,4,6,8,10,11));
+        Lotto lotto = new Lotto(List.of(2,4,6,8,10,12));
+        lotto.receiveBonusNumber(11);
+
+        lotto.winNumber(mockedTickets);
+        assertThat(WinNumberStatus.THREE.getCount()).isEqualTo(1);
+        assertThat(WinNumberStatus.FIVE_BONUS.getCount()).isEqualTo(1);
+    }
 }
